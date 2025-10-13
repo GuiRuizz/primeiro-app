b0ab2b3a8a1e1e62ae7f4f584a05a676a5edb526
GuiRuizz <dsrlikeaboss@gmail.com>
2025-10-13 14:16:25 -0300

    primeiro app react-native

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,6 +30,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="pedidos"
+        options={{
+          title: 'Pedidos',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="video.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
